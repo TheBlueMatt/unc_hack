d0975fe9023662b20fb27e422a1fb0fbd7b38068
Matt Corallo <git@bluematt.me>
2012-11-01 18:41:32 -0400

    Update animal movement

--- projects/zookeeper/web/animals.dart
+++ projects/zookeeper/web/animals.dart
@@ -1,5 +1,6 @@
 library animals;
 import 'boardState.dart';
+import 'dart:math';
 
 /*
  * A generic animal.
@@ -15,14 +16,41 @@ abstract class Animal {
 
 class TheOneTheOnlyAnimal extends Animal {
     int xVelocity, yVelocity;
+    const int VELOCITY = 10;
 
-    TheOneTheOnlyAnimal(BoardState state) {
+    /**
+     * 
+     */
+    TheOneTheOnlyAnimal(BoardState state, int animalNum, int numAnimals) {
         this._state = state;
+        //TODO Decide on const here
+        this.radius = 1;
+        // Start at the middle of the board and move outward in a circle with angles evenly split between each animal
+        this.x = state.WIDTH ~/ 2;
+        this.y = state.HEIGHT ~/ 2;
+        
+        this.xVelocity = (VELOCITY * cos(2*PI * animalNum/numAnimals)).toInt();
+        this.yVelocity = (VELOCITY * sin(2*PI * animalNum/numAnimals)).toInt();
     }
   
     /* Move the animal/update board state */
     void doMove() {
-        // TODO: Implement me!
-        
+        if (this._state.isOutisideOfBlocksArea(x, y)) {
+            //TODO Move like the player (towards the player)
+        } else {
+            this.x += xVelocity;
+            this.y += yVelocity;
+            bool invertV = false;
+            for (int i = 0; i < this.radius; i++) {
+                if (this._state.board[this.x][this.y].state) {
+                    invertV = true;
+                    this._state.board[this.x][this.y].state = false;
+                }
+            }
+            if (invertV) {
+                xVelocity = -xVelocity;
+                yVelocity = -yVelocity;
+            }
+        }
     }
 }
