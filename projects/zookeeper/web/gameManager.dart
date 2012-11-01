018da971a8921f12ddeffb24cdf4bae9caea5bd3
floryst <floryst@somewhere>
2012-11-01 19:02:21 -0400

    added box field generation

--- projects/zookeeper/web/gameManager.dart
+++ projects/zookeeper/web/gameManager.dart
@@ -3,6 +3,7 @@ import 'animals.dart';
 import 'player.dart';
 import 'htmlGen.dart';
 import 'dart:html';
+import 'dart:math';
 
 /**
  * Manages the game logic, starts timers, etc
@@ -22,6 +23,28 @@ class GameManager {
         // Set up our keyboard event handlers.
         // keyDown! Not keyPress!
         document.on.keyDown.add(this.player.handleKeyEvent);
+        
+        // Generate the initial box field.
+        genBoxField();
+    }
+    
+    genBoxField() {
+        // Generate left and white portions of the board.
+        int topLeftX = (boardState.WIDTH~/2) - (boardState.BLOCKS_WIDTH~/2);
+        int topLeftY = (boardState.HEIGHT~/2) - (boardState.BLOCKS_HEIGHT~/2);
+        for (int i = 0; i < boardState.BLOCKS_HEIGHT; i++) {
+            BoardSquare bs = new BoardSquare();
+            bs.state = 1;
+            boardState.board[topLeftX][topLeftY+i] = bs;
+            boardState.board[topLeftX+boardState.BLOCKS_WIDTH-1][topLeftY+i] = bs;
+        }
+        
+        for (int i = 0; i < boardState.BLOCKS_WIDTH; i++) {
+            BoardSquare bs = new BoardSquare();
+            bs.state = 1;
+            boardState.board[topLeftX+i][topLeftY] = bs;
+            boardState.board[topLeftX+i][topLeftY+boardState.BLOCKS_HEIGHT-1] = bs;
+        }
     }
 }
 
